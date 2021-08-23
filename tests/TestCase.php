800b127be14de7304e6de07efb1710c484048eb4
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2021-08-23 06:30:30 +0200

    Add test case class

---
+++ tests/TestCase.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MAKS\Blend\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+}
